3f5caf32dbec683b88c008c04aef967ef532ea46
Kate Bagenzo <katebagenzo@gmail.com>
2025-02-14 12:16:59 +0100

    electron window

---
+++ index.js
@@ -0,0 +1,14 @@
+import {app, BrowserWindow} from 'electron';
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 800
+    });
+
+    win.loadFile('src/index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+})
