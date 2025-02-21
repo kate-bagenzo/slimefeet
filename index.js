af6f50e8d3bb6d5c527195fa55d8a3daea0a74f5
Kate Bagenzo <katebagenzo@gmail.com>
2025-02-21 22:51:16 +0100

    ATTEMPT at steam overlay

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
+const fileURLToPath = require('url');
+const steamworks = require('steamworks.js');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const client = steamworks.init(480);
+console.log(`steam client name is ${client.localplayer.getName()}`);
+app.commandLine.appendSwitch("in-process-gpu");
+app.commandLine.appendSwitch("disable-direct-composition");
+app.allowRendererProcessReuse = false;
+steamworks.electronEnableSteamOverlay(true);
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -16,7 +21,9 @@ const createWindow = () => {
         minHeight: 1000,
         icon: path.join(__dirname, "./src/slimefeet.png"),
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.cjs'),
+            contextIsolation: true,
+            nodeIntegration: true
         }
     });
     //win.removeMenu();
@@ -29,10 +36,9 @@ app.whenReady().then(() => {
 
 ipcMain.handle('quit-app', () => {
     app.quit();
-  });
+});
 
-  ipcMain.handle('fullscreen-app', () => {
+ipcMain.handle('fullscreen-app', () => {
     const win = BrowserWindow.getFocusedWindow();
     win.isFullScreen() ? win.setFullScreen(false) : win.setFullScreen(true);
-  });
-  
+});
